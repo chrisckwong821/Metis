93d95aed7d1148187985192dd4032d9a717f02ce
Waqas Ali <waqas.abbasi@outlook.com>
2017-08-03 10:32:59 +0800

    Azure Table, Speech-to-text

---
+++ Chatbot/DbHandler.js
@@ -0,0 +1,143 @@
+﻿var sql = require('mssql');
+
+var dbSettings = {
+    server: process.env.DB_SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+};
+
+/*
+[
+ {
+     name: Item,
+     value : 'waffles'
+ },
+ {
+     name: Amount,
+     value : 12.0
+ },
+ {
+     name: UserId,
+     value : 923
+ }
+]
+ */
+function insertTransaction(trObject) {
+    var conn = new sql.Connection(dbSettings);
+    conn.connect().then(function () {
+        var transaction = new sql.Transaction(conn);
+        transaction.begin().then(function () {
+            var req = new sql.Request(transaction);
+
+            //creating query string
+            var qString = "Insert into Transactions(";
+            trObject.forEach(function (item) {
+                qString += item.name + ",";
+            });
+            qString = qString.slice(0, -1);
+            qString += ") Values(";
+            trObject.forEach(function (item) {
+                qString += item.value.toString() + ",";
+            });
+            qString = qString.slice(0, -1);
+            qString += ")";
+            //console.log(qString);
+
+            req.query(qString).then(function () {
+                transaction.commit().then(function (recordSet, affected) {
+                    console.log(affected);
+                    conn.close();
+                })
+                    .catch(function (err) {
+                        console.error("Error in committing: " + err);
+                        conn.close();
+                    });
+            })
+                .catch(function (err) {
+                    console.error("Error in query: " + err);
+                    conn.close();
+                });
+        })
+            .catch(function (err) {
+                console.error("Error in begin: " + err);
+                conn.close();
+            });
+    })
+        .catch(function (err) { console.error(err); });
+
+}
+
+function getReport(qrObj, callback) {
+    var conn = new sql.Connection(dbSettings);
+    var req = new sql.Request(conn);
+    conn.connect().then(function () {
+        var query = "Select * from Transactions where UserId='" + qrObj.userId + "'";
+        if (qrObj.startDate) {
+            query += " and DateandTime >= '" + qrObj.startDate + "'";
+        }
+        if (qrObj.endDate) {
+            query += " and DateandTime < '" + qrObj.endDate + "'";
+        }
+        req.query(query).then(function (recordSet) {
+            console.log(recordSet);
+            callback(recordSet);
+        })
+            .catch(function (err) { console.error(err); conn.close(); });
+    })
+        .catch(function (err) { console.error(err); });
+
+}
+
+function getUser(userId, callback) {
+    var conn = new sql.Connection(dbSettings);
+    var req = new sql.Request(conn);
+    conn.connect().then(function () {
+        req.query("Select UserId from Users where UserId='" + userId + "'").then(function (recordSet) {
+            console.log(recordSet);
+            callback(recordSet);
+        })
+            .catch(function (err) { console.error(err); conn.close(); });
+    })
+        .catch(function (err) { console.error(err); });
+
+}
+
+function insertNewUser(usrObj) {
+    var conn = new sql.Connection(dbSettings);
+    conn.connect().then(function () {
+        var transaction = new sql.Transaction(conn);
+        transaction.begin().then(function () {
+            var req = new sql.Request(transaction);
+            req.query("insert into Users(UserId, UserName) Values('" + usrObj.id + "','" + usrObj.name + "')").then(function () {
+                transaction.commit().then(function (recordSet, affected) {
+                    console.log(affected);
+                    conn.close();
+                })
+                    .catch(function (err) {
+                        console.error("Error in committing: " + err);
+                        conn.close();
+                    });
+            })
+                .catch(function (err) {
+                    console.error("Error in query: " + err);
+                    conn.close();
+                });
+        })
+            .catch(function (err) {
+                console.error("Error in begin: " + err);
+                conn.close();
+            });
+    })
+        .catch(function (err) { console.error(err); });
+
+}
+
+
+module.exports.insertTr = insertTransaction;
+module.exports.getReport = getReport;
+module.exports.getUser = getUser;
+module.exports.newUser = insertNewUser;
--- Chatbot/app.js
+++ Chatbot/app.js
@@ -1,7 +1,12 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var url = require('url');
-fs = require('fs');
+var fs = require('fs');
+var azure = require('botbuilder-azure');
+
+var needle = require('needle'), //Speech-to-text
+    ffmpeg = require('fluent-ffmpeg'),
+    speechService = require('./speech-service.js');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -15,11 +20,20 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
+//Setup Azure Table
+var tableName = 'metisbot';
+var azureTableClient = new azure.AzureTableClient(tableName,"metisbot","b9YdNPfchtxl2HhmrRXMBQhnOb3F0jci7VfjzDdbcfnbN9LuhM8Q87BVrO9f2tpu+ElXgm/V5iXw9e/Pj95Vvg==");
+var tableStorage = new azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
 // Initialize a bot
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector).set('storage', tableStorage);;
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
+
+// LUIS Natural Language Processing
+var recognizer = new builder.LuisRecognizer("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/0b06c54f-3ebf-4806-bc8b-da05908fc314?subscription-key=2c282b6ca94042ac891d9b66315517c3&staging=true&verbose=true&timezoneOffset=0&q=");
+bot.recognizer(recognizer);
 
 //=========================================================
 // Bots Global Actions
@@ -53,6 +67,45 @@ function startProactiveDialog(address) {
 }
 
 //=========================================================
+// Bots Middleware
+//=========================================================
+
+bot.use({
+    botbuilder: function (session, next) {
+        if (hasAudioAttachment(session)) {
+            getAudioStreamFromMessage(session.message, function (stream) {
+                speechService.getTextFromAudioStream(stream)
+                    .then(function (text) {
+                        session.message.text = text;
+                        next();
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                        next();
+                    });
+            });
+        } else {
+            next();
+        }
+    }
+});
+
+//=========================================================
+// Bot Start
+//=========================================================
+
+// Send welcome when conversation with bot is started, by initiating the root dialog
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                bot.beginDialog(message.address, '/');
+            }
+        });
+    }
+});
+
+//=========================================================
 // Bots Dialogs
 //=========================================================
 
@@ -69,3 +122,66 @@ bot.dialog('/', function (session) {
 bot.dialog('/WelcomeExistingCustomer', function (session, args, next) {
         session.endDialog('Hello %s, Welcome to the store!',session.message.user.name);
 });
+
+bot.dialog('Specs', function (session, args) {
+    // retrieve hotel name from matched entities
+    var deviceEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'device');
+    if (deviceEntity) {
+        session.send('Looking for specifications of \'%s\'...', deviceEntity.entity);
+        session.endDialog('Dimensions: 11.5" x 7.9" x 0.33" (292 mm x 201 mm x 8.5 mm)\nDisplay	Screen: 12.3" PixelSense Display\nResolution: 2736 x 1824 (267 PPI)\nTouch: 10 point multi- touch\nMemory: 4GB, 8GB, or 16GB RAM\nProcessor: Intel Core 7th- generation m3, i5, or i7\nBattery Life: Up to 13.5 hours of video playback\nGraphics: Intel HD Graphics 615 (m3), Intel HD Graphics 620 (i5), Intel Iris Plus Graphics 640 (i7)');
+    }
+}).triggerAction({
+    matches: 'Specs'
+});
+
+//=========================================================
+// Utilities
+//=========================================================
+
+function hasAudioAttachment(session) {
+    if (session.message.attachments) {
+        return session.message.attachments.length > 0 &&
+            (session.message.attachments[0].contentType === 'audio/wav' ||
+                session.message.attachments[0].contentType === 'application/octet-stream' || session.message.attachments[0].contentType === 'audio/x-m4a' || session.message.attachments[0].contentType === 'audio/aac' || session.message.attachments[0].contentType === 'audio/vnd.dlna.adts');
+    }
+}
+
+
+function getAudioStreamFromMessage(message, cb) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+        });
+    }
+    if (attachment.contentType === 'audio/x-m4a' || attachment.contentType === 'audio/aac' || attachment.contentType === 'audio/vnd.dlna.adts') {
+        headers['Content-Type'] = attachment.contentType;
+        var original = needle.get(attachment.contentUrl, { headers: headers, decode: false });
+        original.on('finish', function () {
+            var command = ffmpeg(original).toFormat('wav');
+            var converted = command.pipe();
+            command
+                .on('error', function (err) {
+                    console.log('An error occurred: ' + err.message);
+                })
+                .on('end', function () {
+                    console.log('Processing finished !');
+                    cb(converted);
+                });
+        });
+    }
+    else {
+        headers['Content-Type'] = attachment.contentType;
+        cb(needle.get(attachment.contentUrl, { headers: headers }));
+    }
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
